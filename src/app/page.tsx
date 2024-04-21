a67d82835bcebbc8d7faa75841e0c2bab148b789
Royweru <royezi17@gmail.com>
2024-04-21 12:16:38 +0300

    modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { supabase } from "@/lib/supabase";
 import { SignOut } from "@/components/sign-out";
 import Image from "next/image";
@@ -6,46 +6,41 @@ import { useArticles } from "@/hooks/useArticles";
 import { useEffect } from "react";
 
 export default function Home() {
-  const{articles,getArticles,subscribeToArticles} = useArticles()
-
+  const { articles, getArticles, subscribeToArticles } = useArticles();
 
   const subscribedChannel = supabase
-  . channel("articles-follow-up")
-  .on("postgres_changes",{
-    event:"*",
-    schema:"public",
-    table:"articles"
-  },(payload:any)=>{
-    console.log(payload)
-  })
-  .subscribe()
+    .channel("articles-follow-up")
+    .on(
+      "postgres_changes",
+      {
+        event: "*",
+        schema: "public",
+        table: "articles",
+      },
+      (payload: any) => {
+        console.log(payload);
+      }
+    )
+    .subscribe();
 
+  const unsubscribeToArticles = () => {
+    supabase.removeChannel(subscribedChannel);
+  };
 
-  const unsubscribeToArticles =  () => {
-    supabase
-    .removeChannel(subscribedChannel)
-}
-
-  useEffect(()=>{
-    
-  
-    getArticles()
-  },[])
+  useEffect(() => {
+    getArticles();
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
       <div>
-         <button onClick={unsubscribeToArticles}>
-             UNSUBSCRIBE
-         </button>
+        <button onClick={unsubscribeToArticles}>UNSUBSCRIBE</button>
       </div>
-       <ul>
-        {articles.map((article:any,key:number)=>{
-          return <li key={key}>
-            {article.title}
-          </li>
+      <ul>
+        {articles.map((article: any, key: number) => {
+          return <li key={key} className=" font-semibold text-2xl">{article.title}</li>;
         })}
-       </ul>
+      </ul>
     </main>
   );
 }
